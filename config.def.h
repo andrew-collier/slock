a9580a57e6fee25f198f2f4c175e1ce8a28c2243
andrew-collier <andrew.collier92@outlook.com>
2022-06-15 11:23:06 -0700

    First commit

---
+++ config.def.h
@@ -0,0 +1,26 @@
+/* user and group to drop privileges to */
+static const char *user  = "drew";
+static const char *group = "wheel";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] = "black",       /* after initialization */
+	[INPUT] = "#005577",    /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "color0",       STRING,  &colorname[INIT] },
+		{ "color4",       STRING,  &colorname[INPUT] },
+		{ "color1",       STRING,  &colorname[FAILED] },
+//		{ "color3",       STRING,  &colorname[CAPS] },
+};
+
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* Background image path, should be available to the user above */
+static const char* background_image = "/home/drew/.cache/wallpaper";
